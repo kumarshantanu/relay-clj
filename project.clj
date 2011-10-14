5b063fa21ec10d243a68fca0914c170100a79265
Shantanu Kumar <kumar.shantanu@gmail.com>
2011-10-14 20:18:58 +0530

    add :warn-on-reflection to project.clj

--- project.clj
+++ project.clj
@@ -1,5 +1,6 @@
 (defproject relay "0.1-SNAPSHOT"
   :description "Data pipelining library"
+  :warn-on-reflection true
   :dependencies []
   :dev-dependencies [[org.clojure/clojure "1.3.0"]]
   :java-source-path "java-src")
